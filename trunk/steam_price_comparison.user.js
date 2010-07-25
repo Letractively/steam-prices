d8e08a8d6d79ba2fb670a863db7e91654a5435f9
torarnelye <torarnelye@a5f91fb0-3418-11de-b310-9106b1f7c239>
2010-07-25 09:54:57 +0000

    Fix compatibility with new Steam store

--- trunk/steam_price_comparison.user.js
+++ trunk/steam_price_comparison.user.js
@@ -80,7 +80,7 @@ var tier2text = "Bosnia and Herzegovina, Bulgaria, Croatia, Cyprus, " +
 //Test the URL to see if we're on a game page
 if (urlGamePattern.test(document.documentURI) || 
       urlPackagePattern.test(document.documentURI)) {
-  someNode = document.getElementById("all");
+  someNode = document.getElementById("global_header");
 
   //For security reasons, JavaScript code isn't allowed to fetch data from
   //external websites. Instead, we insert a HTML <script> tag that fetches
@@ -100,7 +100,7 @@ if (urlGamePattern.test(document.documentURI) ||
   document.body.insertBefore(euscript, someNode);
 
   /* not needed, since price is in USD for the Australian site
-	   but converting to USD to USD will let us change the script easily 
+     but converting to USD to USD will let us change the script easily 
      in case the Australian steam site moves to AUD */
 /*  if (showAUPrice) {
     auscript = document.createElement("script");
@@ -113,7 +113,7 @@ if (urlGamePattern.test(document.documentURI) ||
   //Test to see if the game has a price
   divnodes = document.getElementsByTagName("div");
   for (i=0; i<divnodes.length; i++) {
-    if (divnodes[i].getAttribute("class") == "game_area_purchase_price") {
+    if (divnodes[i].getAttribute("class") == "game_purchase_price price") {
       pricenodes.push(divnodes[i]);
       if (!showTieredEuPrices)
         originalprices.push(divnodes[i].innerHTML);
@@ -130,7 +130,7 @@ if (urlGamePattern.test(document.documentURI) ||
     //dialog from breaking the script
     if (document.cookie.indexOf("birthtime") < 0) { //Check if cookie exists
       var date = new Date();
-		  date.setTime(date.getTime()+(365*24*60*60*1000));//Expires in 365 days
+      date.setTime(date.getTime()+(365*24*60*60*1000));//Expires in 365 days
       document.cookie = "birthtime=1; expires=" //birthtime is set to 1 Jan 1900
         + date.toGMTString() + "; path=/"
     }
@@ -147,7 +147,7 @@ if (urlGamePattern.test(document.documentURI) ||
       ukHttp.open("GET",document.documentURI+"?cc=uk",true);
       ukHttp.send(null);
     }
-	 
+   
     if (showTieredEuPrices) {
       eu1Http = new XMLHttpRequest();
       eu1Http.onreadystatechange=stateChanged;
@@ -158,7 +158,7 @@ if (urlGamePattern.test(document.documentURI) ||
       eu2Http.open("GET",document.documentURI+"?cc="+tier2cc,true);
       eu2Http.send(null);
     }
-	
+  
     if (showAUPrice) {
       auHttp = new XMLHttpRequest();
       auHttp.onreadystatechange=stateChanged;
@@ -166,6 +166,20 @@ if (urlGamePattern.test(document.documentURI) ||
       auHttp.send(null);
     }
 
+    var style = document.createElement("style");
+    style.type = "text/css";
+    document.getElementsByTagName('head')[0].appendChild(style);
+  
+    var s = document.styleSheets[document.styleSheets.length - 1];
+    s.insertRule(".game_area_purchase_game .game_purchase_action{height:auto;bottom:auto}", s.cssRules.length);
+    s.insertRule(".game_purchase_action_bg{height:auto}", s.cssRules.length);
+    s.insertRule(".game_purchase_action  .game_purchase_price{height:auto;padding-bottom:8px}", s.cssRules.length);
+    
+    var margin = 30;
+    if (showUkPrice) margin += 16;
+    if (showTieredEuPrices) margin += 16;
+    if (showAUPrice) margin += 16;
+    s.insertRule(".game_area_purchase_game{margin-bottom:"+margin+"px}", s.cssRules.length);
   }
 }
 
@@ -182,23 +196,23 @@ function stateChanged() {
   
   //The pattern variables can't be reused for some reason, so just duplicate
   var pricepattern0 = 
-    new RegExp(/<div class="game_area_purchase_price">(.+?)<\/div>/gi);
+    new RegExp(/<div class="game_purchase_price price">([^<]+?)<\/div>/gi);
   var pricepattern1 = 
-    new RegExp(/<div class="game_area_purchase_price">(.+?)<\/div>/gi);
+    new RegExp(/<div class="game_purchase_price price">([^<]+?)<\/div>/gi);
   var pricepattern2 = 
-    new RegExp(/<div class="game_area_purchase_price">(.+?)<\/div>/gi);
+    new RegExp(/<div class="game_purchase_price price">([^<]+?)<\/div>/gi);
   var pricepattern3 = 
-    new RegExp(/<div class="game_area_purchase_price">(.+?)<\/div>/gi);
+    new RegExp(/<div class="game_purchase_price price">([^<]+?)<\/div>/gi);
   var pricepattern4 = 
-    new RegExp(/<div class="game_area_purchase_price">(.+?)<\/div>/gi);
+    new RegExp(/<div class="game_purchase_price price">([^<]+?)<\/div>/gi);
   var pricepattern5 = 
-    new RegExp(/<div class="game_area_purchase_price">(.+?)<\/div>/gi);
+    new RegExp(/<div class="game_purchase_price price">([^<]+?)<\/div>/gi);
 
   var priceHtml = new Array(5);
   var mypriceHtml;
-  var usvaluepattern = new RegExp(/&#36;([\d\.]+)$/i);
-  var ukvaluepattern = new RegExp(/&#163;([\d\.]+)$/i);
-  var euvaluepattern = new RegExp(/([\d,-]+)&#8364;$/i);
+  var usvaluepattern = new RegExp(/&#36;([\d\.]+)/i);
+  var ukvaluepattern = new RegExp(/&#163;([\d\.]+)/i);
+  var euvaluepattern = new RegExp(/([\d,-]+)/i);
   var auvaluepattern = new RegExp(/&#36;([\d\.]+)[\s]USD/i);
   var price = new Array(5);
   var myprice;
@@ -208,7 +222,7 @@ function stateChanged() {
       "  var usdConverted; var lessmore; var diff;\n" +
       "  if (currency == 'GBP') {usdConverted = USDtoGBP(usdPrice);}\n" +
       "  else if (currency == 'EUR') {usdConverted = USDtoEUR(usdPrice);}\n" +
-//	  "  else if (currency == 'USD') {usdConverted = USDtoUSD(usdPrice);}\n" +
+//    "  else if (currency == 'USD') {usdConverted = USDtoUSD(usdPrice);}\n" +
       "  else if (currency == 'USD') {usdConverted = usdPrice;}\n" +
       "  diff = Math.abs((localPrice/usdConverted)*100-100);\n" +
       "  if (localPrice >= usdConverted) {lessmore = 'higher';}\n" +
@@ -219,11 +233,11 @@ function stateChanged() {
   calculatescript.setAttribute("type", "text/javascript");
   calculatescript.innerHTML = calcscript;
   document.body.insertBefore(calculatescript, someNode);
-    
+  
   for (i=0; i<pricenodes.length; i++) {
     if (!showTieredEuPrices) {
       try {
-        var myvaluepattern = new RegExp(/([\d,-]+).$/i);
+        var myvaluepattern = new RegExp(/([\d,-]+)/i);
         mypriceHtml = originalprices[i];
         myprice = parseFloat(myvaluepattern.exec(originalprices[i]
           )[1].replace(",", ".").replace("--", "00"));
@@ -245,6 +259,10 @@ function stateChanged() {
       }
     }
     catch(err) {
+      //Prevent search from looping around and starting at the beginning
+        if (err.message.search("responseText\\) is null") != -1) {
+          usHttp = null; priceHtml[0] = "N/A";
+        }
       if (!priceHtml[0] || priceHtml[0].length == 0)
         priceHtml[0] = "N/A";
       price[0] = null;
@@ -259,6 +277,10 @@ function stateChanged() {
         price[1] = parseFloat(ukvaluepattern.exec(priceHtml[1])[1]);
       }
       catch(err) {
+        //Prevent search from looping around and starting at the beginning
+        if (err.message.search("responseText\\) is null") != -1) {
+          ukHttp = null; priceHtml[1] = "N/A";
+        }
         if (!priceHtml[1] || priceHtml[1].length == 0)
           priceHtml[1] = "N/A";
         price[1] = null;
@@ -269,12 +291,16 @@ function stateChanged() {
       createGetDifferenceScript("gbp" + i, "GBP", price[0], price[1]);
     }
 
-	
+  
     if (showTieredEuPrices) {
       try {
         priceHtml[2] = pricepattern3.exec(eu1Http.responseText)[1];
       }
       catch(err) {
+        //Prevent search from looping around and starting at the beginning
+        if (err.message.search("responseText\\) is null") != -1) {
+          eu1Http = null; priceHtml[2] = "N/A";
+        }
         if (!priceHtml[2] || priceHtml[2].length == 0)
           priceHtml[2] = "N/A";
       }
@@ -282,6 +308,10 @@ function stateChanged() {
         priceHtml[3] = pricepattern4.exec(eu2Http.responseText)[1];
       }
       catch(err) {
+        //Prevent search from looping around and starting at the beginning
+        if (err.message.search("responseText\\) is null") != -1) {
+          eu2Http = null; priceHtml[3] = "N/A";
+        }
         if (!priceHtml[3] || priceHtml[3].length == 0)
           priceHtml[3] = "N/A";
       }
@@ -312,13 +342,17 @@ function stateChanged() {
             + " <span id='myprice" + i + "'></span>";
       createGetDifferenceScript("myprice" + i, "EUR", price[0], myprice);
     }
-	
+  
     if (showAUPrice) {
       try {
         priceHtml[4] = pricepattern5.exec(auHttp.responseText)[1];
         price[4] = parseFloat(auvaluepattern.exec(priceHtml[4])[1]);
       }
       catch(err) {
+        //Prevent search from looping around and starting at the beginning
+        if (err.message.search("responseText\\) is null") != -1) {
+          auHttp = null; priceHtml[4] = "N/A";
+        }
         if (!priceHtml[4] || priceHtml[4].length == 0)
           priceHtml[4] = "N/A";
         price[4] = null;
